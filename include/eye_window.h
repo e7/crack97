c785a2b6e20906bac8eb4706a85cdc98a0b8405d
e7 <jackzxty@126.com>
2016-07-29 11:48:55 +0800

    增加噪声处理

--- include/eye_window.h
+++ include/eye_window.h
@@ -9,10 +9,100 @@
 #include <QDesktopWidget>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "ui_main_window.h"
 #include "singleton.h"
+
+
+// static QImage Mat2QImage(cv::Mat const& src)
+// {
+//      cv::Mat temp; // make the same cv::Mat
+//      cvtColor(src, temp,CV_BGR2RGB); // cvtColor Makes a copt, that what i need
+//      QImage dest((const uchar *) temp.data, temp.cols, temp.rows, temp.step, QImage::Format_RGB888);
+//      dest.bits(); // enforce deep copy, see documentation
+//      // of QImage::QImage ( const uchar * data, int width, int height, Format format )
+//      return dest;
+// }
+//
+// static cv::Mat QImage2Mat(QImage const& src)
+// {
+//      cv::Mat tmp(src.height(),src.width(),CV_8UC3,(uchar*)src.bits(),src.bytesPerLine());
+//      cv::Mat result; // deep copy just in case (my lack of knowledge with open cv)
+//      cvtColor(tmp, result,CV_BGR2RGB);
+//      return result;
+// }
+
+
+static QImage Mat2QImage(const cv::Mat& mat)
+{
+    // 8-bits unsigned, NO. OF CHANNELS = 1
+    if(mat.type() == CV_8UC1)
+    {
+        QImage image(mat.cols, mat.rows, QImage::Format_Indexed8);
+        // Set the color table (used to translate colour indexes to qRgb values)
+        image.setColorCount(256);
+        for(int i = 0; i < 256; i++)
+        {
+            image.setColor(i, qRgb(i, i, i));
+        }
+        // Copy input Mat
+        uchar *pSrc = mat.data;
+        for(int row = 0; row < mat.rows; row ++)
+        {
+            uchar *pDest = image.scanLine(row);
+            memcpy(pDest, pSrc, mat.cols);
+            pSrc += mat.step;
+        }
+        return image;
+    }
+    // 8-bits unsigned, NO. OF CHANNELS = 3
+    else if(mat.type() == CV_8UC3)
+    {
+        // Copy input Mat
+        const uchar *pSrc = (const uchar*)mat.data;
+        // Create QImage with same dimensions as input Mat
+        QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
+        return image.rgbSwapped();
+    }
+    else if(mat.type() == CV_8UC4)
+    {
+        qDebug() << "CV_8UC4";
+        // Copy input Mat
+        const uchar *pSrc = (const uchar*)mat.data;
+        // Create QImage with same dimensions as input Mat
+        QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_ARGB32);
+        return image.copy();
+    }
+    else
+    {
+        qDebug() << "ERROR: Mat could not be converted to QImage.";
+        return QImage();
+    }
+}
+
+
+static cv::Mat QImage2Mat(QImage image)
+{
+    cv::Mat mat;
+    qDebug() << image.format();
+    switch(image.format())
+    {
+    case QImage::Format_ARGB32:
+    case QImage::Format_RGB32:
+    case QImage::Format_ARGB32_Premultiplied:
+        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
+        break;
+    case QImage::Format_RGB888:
+        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
+        cv::cvtColor(mat, mat, CV_BGR2RGB);
+        break;
+    case QImage::Format_Indexed8:
+        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
+        break;
+    }
+    return mat;
+}
 
 
 class QEyeWindow : public QMainWindow
@@ -32,49 +122,9 @@ private:
     Ui::MainWindow ui;
 
 private slots:
-    void onOpenImage(void)
-    {
-        QString file_name(QFileDialog::getOpenFileName(
-            this, tr("open image"), "/", tr("image files (*.png *.jpg)")
-        ));
-
-        if (0 == file_name.size()) {
-            return;
-        }
-
-        qDebug(file_name.toAscii().data());
-
-        QImage *image = new QImage(file_name);
-        QScopedPointer<QImage> free_image(image);
-
-        this->label_img->setPixmap(QPixmap::fromImage(*image));
-        this->label_img->resize(QSize(image->width(), image->height()));
-    }
-
-    void onScreenShot(void)
-    {
-        // 截屏
-        QPixmap screen(QPixmap::grabWindow(QApplication::desktop()->winId()));
-        this->label_img->setPixmap(screen);
-    }
-
-    void onToggleTimer(void)
-    {
-        if (this->screen_shot_timer->isActive()) {
-            this->screen_shot_timer->stop();
-        } else {
-            this->screen_shot_timer->start(this->timer_resolution);
-        }
-    }
-
-    void onProcess(void)
-    {
-        // 控制滚动条
-        int cur_val = ui.progress_bar->value();
-
-        if (cur_val < ui.progress_bar->maximum()) {
-            ui.progress_bar->setValue(cur_val + 1);
-        }
-    }
+    void onOpenImage(void);
+    void onScreenShot(void);
+    void onToggleTimer(void);
+    void onProcess(void);
 };
 #endif // __CRACK97_EYE_WINDOW_H__
--- src/eye_window.cpp
+++ src/eye_window.cpp
@@ -35,7 +35,68 @@ QEyeWindow::QEyeWindow(void)
         this->screen_shot_timer,
         SIGNAL(timeout(void)), this, SLOT(onScreenShot(void))
     );
-    this->screen_shot_timer->start(this->timer_resolution);
 
     show();
+}
+
+void QEyeWindow::onOpenImage(void)
+{
+    QString file_name(QFileDialog::getOpenFileName(
+        this, tr("open image"), "/", tr("image files (*.png *.jpg)")
+    ));
+
+    if (0 == file_name.size()) {
+        return;
+    }
+
+    qDebug(file_name.toAscii().data());
+
+    QImage *image = new QImage(file_name);
+    QScopedPointer<QImage> free_image(image);
+
+    this->label_img->setPixmap(QPixmap::fromImage(*image));
+    this->label_img->resize(QSize(image->width(), image->height()));
+
+	return;
+}
+
+
+void QEyeWindow::onScreenShot(void)
+{
+    // 截屏
+    QPixmap screen(QPixmap::grabWindow(QApplication::desktop()->winId()));
+    this->label_img->setPixmap(screen);
+
+	return;
+}
+
+
+void QEyeWindow::onToggleTimer(void)
+{
+    if (this->screen_shot_timer->isActive()) {
+        this->screen_shot_timer->stop();
+    } else {
+        this->screen_shot_timer->start(this->timer_resolution);
+    }
+}
+
+
+void QEyeWindow::onProcess(void)
+{
+    // 控制滚动条
+    // int cur_val = ui.progress_bar->value();
+
+    // if (cur_val < ui.progress_bar->maximum()) {
+    //     ui.progress_bar->setValue(cur_val + 1);
+    // }
+
+    QImage curt_img(this->label_img->pixmap()->toImage());
+    cv::Mat curt_mat(QImage2Mat(curt_img));
+    cv::Mat mat_noise;
+
+    medianBlur(curt_mat, mat_noise, 3);
+
+    QImage img_noise(Mat2QImage(mat_noise));
+    this->label_img->setPixmap(QPixmap::fromImage(img_noise));
+    this->label_img->resize(QSize(img_noise.width(), img_noise.height()));
 }
